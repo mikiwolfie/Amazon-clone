b915bbb79293147fd1eaa089bda83cfd17c328b4
unknown <samanthamikhail24@gmail.com>
2021-02-22 07:18:39 +0800

    amazon completion

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBR7TDrBTcYV3Ay9IvXJhSjNbXy3thtn50",
+    authDomain: "challenge-cba89.firebaseapp.com",
+    projectId: "challenge-cba89",
+    storageBucket: "challenge-cba89.appspot.com",
+    messagingSenderId: "980269310588",
+    appId: "1:980269310588:web:4d1fca5af11ef41f015ff9",
+    measurementId: "G-99VKTLFKHB"
+};
