4fc6cd4e756ffa9f79f2cf7451927a3a2d237dc4
unknown <>
2021-02-23 12:04:02 +0800

    amazon clone completion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import './App.css';
-import Header from './Header.js'
-import Home from "./Home.js"
+import Header from './Header.js';
+import Home from "./Home.js";
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
+import Checkout from "./Checkout";
 
 
 function App() {
-    return (
-      // BEM
-      <Router>
-      <div className = "app" >
-      <Switch>
-       <Header />
-       <Home />
-      </Switch>
+  return (
+    // BEM
+    <Router>
+      <div className = "app">
+      <Header />
+
+        <Switch>
+          <Route path = "/checkout">
+            <Checkout />
+          </Route>
+
+          <Route path = "/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      </Router>
-    );
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker"
 
 
 ReactDOM.render(
   <React.StrictMode>
+   <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+   </StateProvide>
   </React.StrictMode>,
   document.getElementById('root')
 );
