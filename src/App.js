9ee7d04bb194afae80e372838282050838c76c06
unknown <>
2021-03-09 06:59:38 +0800

    amazon completion

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import { BrowserRouter as Router, Switch, Route } from
 "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
 
+const promise = loadStripe(
+ "pk_test_51ISZRjIv9jWTDXdN52FWwLXH4dK7e0gH3q89KBtqYYsgqUFHmXQNgdxN9J227JEtA62EQauxQP8v18hf2Da9nh5h00ZDMktQHq"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -47,6 +53,12 @@ function App() {
           <Route path ="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path ="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
---
+++ src/Payment.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const stripe = useStripe(); 
+  const elements = useElements();
+
+  const handleSubmit = e => {
+    // do all the fancy stripe stuff...
+  }
+
+  const handleChange = e => {
+
+  }
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+      <h1>
+        Checkout (
+          <Link to="/checkout">{basket?.length} items</Link>
+        )
+      </h1>
+
+
+        {/* Payment section - delivery address */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        {/* Payment section - Review Items */}
+        <div className="payment__section">
+          <div className="payment-title">
+             <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Payment section - Payment method */}
+        <div className="payment__section">
+        <div className="payment__title">
+          <h3>Payment Method</h3>
+        </div>
+          <div className="payment__details">
+            {/* Stripe magic will go*/}
+
+              <form onSubmit={handleSubmit}>
+                  <CardElement onChange={handleChange}/>
+              </form>
+            </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Payment;
